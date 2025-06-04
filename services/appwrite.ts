78a5a6a74f871f389a7e3c64d97fc2fe59404fe6
Muhamad Zaky Ramadhan <zakyramadhanxstudy@gmail.com>
2025-06-04 20:18:21 +0700

    Update appwrite.ts

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -4,49 +4,48 @@ const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
 const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
 const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
+  .setEndpoint('https://cloud.appwrite.io/v1')
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
 
 const database = new Databases(client);
 
 export const updateSearchCount = async (query: string, movie: Movie) => {
-    try {
-        const result = await database.listDocuments(
-            DATABASE_ID,
-            COLLECTION_ID,
-            [Query.equal('searchTerm', query)]
-        );
+  try {
+    const result = await database.listDocuments(
+      DATABASE_ID,
+      COLLECTION_ID,
+      [Query.equal('searchTerm', query)]
+    );
 
-        if (result.documents.length > 0) {
-            const existingMovie = result.documents[0];
-            await database.updateDocument(
-                DATABASE_ID,
-                COLLECTION_ID,
-                existingMovie.$id,
-                {
-                    count: existingMovie.count + 1,
-                }
-            );
-        } else {
-            await database.createDocument(
-                DATABASE_ID,
-                COLLECTION_ID,
-                ID.unique(),
-                data,
-                permissions
-                {
-                    searchTerm: query,
-                    movie_id: movie.id,
-                    title: movie.title,
-                    count: 1,
-                    poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
-                }),
-            ['read("role:all")', 'write("role:all")']
-        };
-    } catch (error) {
-        console.error('Error updating search count:', error);
-        throw error;
+    if (result.documents.length > 0) {
+      const existingMovie = result.documents[0];
+      await database.updateDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        existingMovie.$id,
+        {
+          count: existingMovie.count + 1,
+        }
+      );
+    } else {
+      await database.createDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        ID.unique(),
+        {
+          searchTerm: query,
+          movie_id: movie.id,
+          title: movie.title,
+          count: 1,
+          poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+        },
+        ['read("role:all")', 'write("role:all")'] // permissions harus di sini
+      );
     }
+  } catch (error) {
+    console.error('Error updating search count:', error);
+    throw error;
+  }
 };
 
 export const getTrendingMovies = async (): Promise<
