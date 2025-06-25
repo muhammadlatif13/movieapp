2dceee683d494b35b8a105749d9dd9181f4d9b0d
muhammadlatif13 <latifmua@gmail.com>
2025-06-26 01:04:09 +0700

    ...

--- backend_movieapp/config/db.js
+++ backend_movieapp/config/db.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
-  database: "movieapp",
+  password: "", // Sesuain sama pass db
+  database: "movieapp", // Sesuain sama nama db
 });
 
 db.connect((err) => {
